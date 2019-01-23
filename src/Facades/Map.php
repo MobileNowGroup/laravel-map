c5985f0c3aad12bff672abbedb35e637cae06cd7
Fayne Guo <fayne.guo@gmail.com>
2019-01-23 21:10:39 +0800

    :+1: Make facades more friendly

--- src/Facades/Map.php
+++ src/Facades/Map.php
@@ -4,6 +4,11 @@ namespace MobileNowGroup\LaravelMap\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array getCoordinates(string $address, $city = null)
+ *
+ * @see \MobileNowGroup\LaravelMap\Map
+ */
 class Map extends Facade
 {
     /**
